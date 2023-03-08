84fc318a6daa50ade676f5fc9f9e33ad066296cb
zaferguvenn <zafer.guven58@gmail.com>
2023-03-08 21:30:47 +0300

    scientists and inventions

--- app/src/main/java/com/lafimsize/bilimnsanlarvebulular/adapter/VPAdapter.kt
+++ app/src/main/java/com/lafimsize/bilimnsanlarvebulular/adapter/VPAdapter.kt
@@ -61,7 +61,7 @@ class VPAdapter(val cImages:ArrayList<Int>):RecyclerView.Adapter<VPAdapter.VPVie
                 binding.slideIV.setImageResource(cImages.get(p))
                 val randomAnnimation=annimationsList.random()
                 binding.slideIV.startAnimation(randomAnnimation)
-                p=p+1
+                p=p+2
                 val randomTime=(4000L..8000L).random()
                 delay(randomTime)
 
